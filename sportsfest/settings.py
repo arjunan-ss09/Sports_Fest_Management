5c0806cb9203121975793e89282bc6a12ab4b216
arjunan-ss09 <f20241246@pilani.bits-pilani.ac.in>
2025-04-09 16:32:19 +0530

    Update settings.py

--- sportsfest/settings.py
+++ sportsfest/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-64r3qyuz5daa#ytatzxn)no5uu#acmctynj1fx13qljxf!kwl3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
